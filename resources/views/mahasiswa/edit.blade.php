b7f0e91d13ff7bd1975984436bebb3ce4cff5e4b
Vyinnf <moh.bustanul.arifin12@gmail.com>
2025-05-20 00:10:48 +0700

    Update UI Edit Profile Mahasiswa

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -1,4 +1,5 @@
 {{-- data mahasiswa --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <link rel="stylesheet" href="{{ asset('assets/css/edit.css') }}">
 
 {{-- Alert success & error --}}
@@ -21,16 +22,16 @@
 
 <form class="forms-sample" method="POST" action="{{ route('mahasiswa.profil.update') }}" enctype="multipart/form-data">
     @csrf
+    <h4 class="card-title"><span>Data Diri Mahasiswa</span></h4>
+
     <div class="row mt-4">
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title"><span>Data Diri Mahasiswa</span></h4>
-
                     {{-- Foto --}}
                     <div class="form-group">
-                        <label for="foto">Foto Profil</label>
+                        <label for="foto"><i class="fa fa-image"></i> Foto Profil</label>
                         <div class="mb-2">
                             <img id="preview"
                                 src="{{ $mahasiswa->foto ? asset('uploads/foto/' . $mahasiswa->foto) : 'https://via.placeholder.com/100' }}"
@@ -45,7 +46,7 @@
 
                     {{-- Nama --}}
                     <div class="form-group">
-                        <label for="nama">Nama Lengkap</label>
+                        <label for="nama"><i class="fa fa-user"></i> Nama Lengkap</label>
                         <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama"
                             name="nama" placeholder="Masukkan nama lengkap"
                             value="{{ old('nama', $mahasiswa->nama ?? '') }}">
@@ -56,7 +57,7 @@
 
                     {{-- Email --}}
                     <div class="form-group">
-                        <label for="email">Email</label>
+                        <label for="email"><i class="fa fa-envelope"></i> Email</label>
                         <input type="email"
                             class="form-control @error('email') is-invalid @enderror"
                             id="email"
@@ -70,7 +71,7 @@
 
                     {{-- NIM --}}
                     <div class="form-group">
-                        <label for="nim">NIM</label>
+                        <label for="nim"><i class="fa fa-id-card"></i> NIM</label>
                         <input type="text" class="form-control @error('nim') is-invalid @enderror" id="nim"
                             name="nim" placeholder="Masukkan NIM" value="{{ old('nim', $mahasiswa->nim ?? '') }}">
                         @error('nim')
@@ -80,7 +81,7 @@
 
                     {{-- Semester --}}
                     <div class="form-group">
-                        <label for="semester">Semester</label>
+                        <label for="semester"><i class="fa fa-layer-group"></i> Semester</label>
                         <select class="form-control @error('semester') is-invalid @enderror" id="semester"
                             name="semester">
                             @for ($i = 1; $i <= 14; $i++)
@@ -97,7 +98,7 @@
 
                     {{-- Prodi --}}
                     <div class="form-group">
-                        <label for="prodi">Program Studi</label>
+                        <label for="prodi"><i class="fa fa-graduation-cap"></i> Program Studi</label>
                         <input type="text" class="form-control @error('prodi') is-invalid @enderror" id="prodi"
                             name="prodi" placeholder="Masukkan nama prodi"
                             value="{{ old('prodi', $mahasiswa->prodi ?? '') }}">
@@ -108,7 +109,7 @@
 
                     {{-- Tempat Lahir --}}
                     <div class="form-group">
-                        <label for="tempat_lahir">Tempat Lahir</label>
+                        <label for="tempat_lahir"><i class="fa fa-map-marker-alt"></i> Tempat Lahir</label>
                         <input type="text" class="form-control @error('tempat_lahir') is-invalid @enderror"
                             id="tempat_lahir" name="tempat_lahir" placeholder="Masukkan tempat lahir"
                             value="{{ old('tempat_lahir', $tempat_lahir ?? '') }}">
@@ -119,7 +120,7 @@
 
                     {{-- Tanggal Lahir --}}
                     <div class="form-group">
-                        <label for="tanggal_lahir">Tanggal Lahir</label>
+                        <label for="tanggal_lahir"><i class="fa fa-calendar-alt"></i> Tanggal Lahir</label>
                         <input type="date" class="form-control @error('tanggal_lahir') is-invalid @enderror"
                             id="tanggal_lahir" name="tanggal_lahir" value="{{ old('tanggal_lahir', $tanggal_lahir ?? '') }}">
                         @error('tanggal_lahir')
@@ -129,7 +130,7 @@
 
                     {{-- Alamat --}}
                     <div class="form-group">
-                        <label for="alamat">Alamat Lengkap</label>
+                        <label for="alamat"><i class="fa fa-home"></i> Alamat Lengkap</label>
                         <textarea class="form-control @error('alamat') is-invalid @enderror" id="alamat" name="alamat" rows="3"
                             placeholder="Masukkan alamat lengkap">{{ old('alamat', $mahasiswa->alamat ?? '') }}</textarea>
                         @error('alamat')
@@ -139,9 +140,15 @@
 
                     {{-- Tombol --}}
                     <div class="form-actions mt-3">
-                        <a href="{{ route('mahasiswa.dashboard') }}" class="btn btn-secondary">Back</a>
-                        <button type="reset" class="btn btn-light">Reset</button>
-                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <a href="{{ route('mahasiswa.dashboard') }}" class="btn btn-secondary">
+                            <i class="fa fa-arrow-left"></i> Back
+                        </a>
+                        <button type="reset" class="btn btn-light">
+                            <i class="fa fa-rotate-left"></i> Reset
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-save"></i> Simpan
+                        </button>
                     </div>
 
                 </div>
